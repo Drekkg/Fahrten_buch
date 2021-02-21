3c8f7264d543b234f75f648be4bb9a04355330f9
Drekkg <77752848+Drekkg@users.noreply.github.com>
2021-02-21 16:07:04 +0100

    Add files via upload

---
+++ Fahrten buch.py
@@ -0,0 +1,12 @@
+import random
+
+kilometer = int(input("Kilometer stand: "))
+# kilometer_driven_daily = random.randint(20, 45)
+day = 1
+while True:
+    kilometer_driven_daily = random.randint(52, 89)  # random number of average driven kilometers
+    kilometer = kilometer - kilometer_driven_daily
+    day += 1
+    print("Neue Kilometer Stand ist {}:  Kilometer gefahren {}".format(kilometer, kilometer_driven_daily))
+    print(day)
+    input()

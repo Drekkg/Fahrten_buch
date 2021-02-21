eaed4ba961560e3631cddf546e3e8776ba61dfdd
Drekkg <77752848+Drekkg@users.noreply.github.com>
2021-02-21 16:16:50 +0100

    Update Fahrten buch.py
    changed it to add driven kilometres instead of subtracting

--- Fahrten buch.py
+++ Fahrten buch.py
@@ -5,7 +5,7 @@ kilometer = int(input("Kilometer stand: "))
 day = 1
 while True:
     kilometer_driven_daily = random.randint(52, 89)  # random number of average driven kilometers
-    kilometer = kilometer - kilometer_driven_daily
+    kilometer = kilometer + kilometer_driven_daily
     day += 1
     print("Neue Kilometer Stand ist {}:  Kilometer gefahren {}".format(kilometer, kilometer_driven_daily))
     print(day)
